7fdf28a83d9f80a8412124de6a359707b2dbcd5e
mekeirc <mekeirc@gmail.com>
2025-09-29 02:10:59 +0100

    Problem solvers area sorted

---
+++ alt-agency-test/app/components/problemsolvers.tsx
@@ -0,0 +1,12 @@
+export default function ProblemSolvers() {
+	return (
+		<div className="container mx-auto bg-black w-full pt-20 pb-30 flex flex-col items-center text-white">
+			<h3 className="text-[40px] font-bold">The business process problem solvers.</h3>
+			<div className="flex font-plexmono gap-[78px] mt-8">
+				<p className="w-[470px]">Recognising your frustrations with your print environment, IT services, document management & communications and finding a solution to overcome them.  Recognising your frustrations with your print environment, IT services, document management & communications and finding a solution to overcome them. </p>
+				<p className="w-[470px]">Recognising your frustrations with your print environment, IT services, document management & communications and finding a solution to overcome them.  Recognising your frustrations with your print environment, IT services, document management & communications and finding a solution to overcome them. </p>
+			</div>
+		</div>
+	);
+}
+
--- alt-agency-test/app/page.tsx
+++ alt-agency-test/app/page.tsx
@@ -2,15 +2,17 @@ import Hero from "./components/hero";
 import LogoArea from "./components/logoarea";
 import Process from "./components/process";
 import CaseStudies from "./components/casestudies";
+import ProblemSolvers from "./components/problemsolvers";
 
 export default function Home() {
   return (
     <div>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col row-start-2 items-center sm:items-start">
           <Hero />
           <LogoArea />
           <Process />
           <CaseStudies />
+          <ProblemSolvers />
       </main>
     </div>
   );
